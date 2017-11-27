dd9fbb8a7310590793be921803c0a0aae0ae0582
chromevoid <chromevoid500@gmail.com>
2017-11-26 20:12:53 -0500

    Add some comments

--- src/edu/nyu/TransactionManager.java
+++ src/edu/nyu/TransactionManager.java
@@ -1,9 +1,15 @@
 package edu.nyu;
 
 /**
- * Transcation Manager.
+ * Transaction Manager:
+ *  A single transaction manager
+ *  that translates read and write requests on variables to read and write requests
+ *  on copies using the available copy algorithm described in the notes.
+ *  The transaction manager never fails.
+ *  https://cs.nyu.edu/courses/fall17/CSCI-GA.2434-001/
  */
 public class TransactionManager {
+
     void runTest(String input) {
         System.out.println(input);
     }
