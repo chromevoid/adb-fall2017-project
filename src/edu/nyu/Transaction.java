d52225f50f0b0294ad8f8f8eaf929f1594b8961e
Mona Qiu <mq438@nyu.edu>
2017-11-30 17:02:33 -0500

    Add Commit transaction function

--- src/edu/nyu/Transaction.java
+++ src/edu/nyu/Transaction.java
@@ -1,22 +1,16 @@
 package edu.nyu;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Transaction {
     List<Lock> locks;
-    //TODO: this needs change
-    Map<String , Variable> writeVariables;
-
     List<History> transactionHistory;
     int versionNumber;
     String transactionName;
 
     public Transaction(int versionNumber, String transactionName) {
         locks = new ArrayList<>();
-        writeVariables = new HashMap<>();
         transactionHistory = new ArrayList<>();
         this.versionNumber = versionNumber;
         this.transactionName = transactionName;
