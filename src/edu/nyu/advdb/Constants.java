ee820f98d0476bb4f4f23ed48ea0a26e8f2f0319
Mona Qiu <mq438@nyu.edu>
2017-12-02 12:23:03 -0500

    extract constant vaules to the Constants class

---
+++ src/edu/nyu/advdb/Constants.java
@@ -0,0 +1,14 @@
+package edu.nyu.advdb;
+
+public class Constants {
+    public final static String READ_OPERATION = "R";
+    public final static String WRITE_OPERATION = "W";
+
+    public final static String READ_LOCK = "read";
+    public final static String WRITE_LOCK = "write";
+
+    public final static int VARIABLE_AMOUNT = 20;
+    public final static int SITE_AMOUNT = 10;
+
+    public final static int READ_WRITE_TRANSACTION_VERSION = -1;
+}
